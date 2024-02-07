a60a86838d1f0a0bde8183f733830db7e7dade51
Luis Mendes <machadoluismendes@gmail.com>
2024-02-07 14:32:32 -0300

    Increment
    http://menn-test-2024.vercel.app/

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 var router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
 
+const axios = require("axios");
+
+const options = { 
+  method: "GET",
+  url: "http://menn-test-2024.vercel.app/",
+  headers: { "authorization": "Bearer TOKEN" },
+};
+
+axios(options)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Auth0 Webapp sample Nodejs',
