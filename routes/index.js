8d3cfcafe9d9c62aaf49be42a40f8425acc54b6c
Luis Mendes <machadoluismendes@gmail.com>
2024-02-28 15:24:29 -0300

    Increment
    env

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var router = require('express').Router();
+const router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
 
 const axios = require("axios");
